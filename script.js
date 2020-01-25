69d9c5bc2afd2d2683a63533128232ef8711c1c3
Jana Laney <Jana.Laney@gmail.com>
2020-01-25 11:40:23 -0700

    de-bugged errors

--- script.js
+++ script.js
@@ -3,29 +3,28 @@ console.log("javascript loaded")
 var timeBlock = document.querySelector(".input-group");
 var saveEntry = document.querySelector(".fa-save");
 
+var timer = setInterval(showColor, 60000)
+
 var plan = JSON.parse(localStorage.getItem("plan"))  // store key and a string
 if (!plan) {
     plan = {}
 }
 
 
-
 function showInfo() {
-
     for (key in plan) {
         console.log(key, plan[key])
         $("#" + key).val(plan[key])   // $("#msg8")
     }
 }
 
-
 $(".input-group-addon").on("click", function () {
     console.log("click the whole button")
 
     event.preventDefault();
     console.log("clicked")
-    var input = $(this).siblings(".form-control").val()
-    var hour = $(this).siblings(".form-control").attr("id")
+    var input = $(this).siblings(".form-Control").val()
+    var hour = $(this).siblings(".form-Control").attr("id")
     console.log(input, hour)
 
     // save on localstorage
@@ -38,11 +37,27 @@ $(".input-group-addon").on("click", function () {
 // change the colors of the input area based on the current time
 function showColor() {
     // get the current time  // use moment
-    var current = moment().format("dddd, MMMM Do")
+    var current = moment().format("MMMM Do YYYY, h:mm a")
     $("#currentDay").text(current)
     var hour = moment().hours()
+    console.log("current hour", hour)
 
-    //
+    //  msg8  past present future
+    $(".form-Control").each(function () {
+        var id = $(this).attr("id")
+        var time = parseInt(id.slice(3))
+        console.log("time", time)
+
+        if (time < hour) {
+            $(this).addClass("past")
+        }
+        else if (time === hour) {
+            $(this).addClass("present")
+        }
+        else {
+            $(this).addClass("future")
+        }
+    })
 }
 
 //create function to save content
@@ -50,9 +65,6 @@ function timeBlock(type, message) {
     msgDiv.textContent = message;
     msgDiv.setAttribute("class", type);
 }
-
-
-
 
 showInfo()
 showColor()
